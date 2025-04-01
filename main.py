5a10781cd8357bb61bb5bab2e1921fe00af2756e
Rajesh4Github <112881613+Rajesh4Github@users.noreply.github.com>
2025-04-01 21:46:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -188,12 +188,12 @@ async def receive_question(question: str = Form(...), file: UploadFile = File(No
         answer = func_answer or await read_answer(task_id=task_id, question=question)
     elif task_id in ["GA1.13"]:
         answer = GA1_13(question)
-        # answer = "https://raw.githubusercontent.com/Telvinvarghese/Test/main/email.json"
+        # answer = "https://raw.githubusercontent.com/Rajesh4Github/Test/main/email.json"
     elif task_id in ["GA2.1"]:
         answer = await read_answer(task_id=task_id, question=question)
     elif task_id in ["GA2.3"]:
         # answer = GA2_3(question)
-        answer = "https://tusharisme.github.io/tds_work"
+        answer = "https://Rajesh4Github.github.io/tds_work"
     elif task_id in ["GA2.2", "GA2.4"]:
         if file:
             print(file)
@@ -218,20 +218,20 @@ async def receive_question(question: str = Form(...), file: UploadFile = File(No
                 task_id=task_id, question=question, file_path=""
             )
     elif task_id in ["GA2.6"]:
-        answer = "https://api-git-main-telvinvargheses-projects.vercel.app/api"
+        answer = "https://api-git-main-Rajesh4Githubs-projects.vercel.app/api"
         # print(file)
         # if file:
         #     flag = await GA2_6_file(file)
         #     if flag == "True":
-        #         answer = "https://api-git-main-telvinvargheses-projects.vercel.app/api"
+        #         answer = "https://api-git-main-Rajesh4Githubs-projects.vercel.app/api"
         #     else:
-        #         answer = "https://api-git-main-telvinvargheses-projects.vercel.app/api"
+        #         answer = "https://api-git-main-Rajesh4Githubs-projects.vercel.app/api"
         # else:
-        #     answer = "https://api-git-main-telvinvargheses-projects.vercel.app/api"
+        #     answer = "https://api-git-main-Rajesh4Githubs-projects.vercel.app/api"
     elif task_id in ["GA2.7"]:
-        answer = "https://github.com/Tusharisme/pyth"
+        answer = "https://github.com/Rajesh4Github/pyth"
         # answer = GA2_7(question)
-        # answer = "https://github.com/Telvinvarghese/Test"
+        # answer = "https://github.com/Rajesh4Github/Test"
     elif task_id in ["GA2.8"]:
         answer = "https://hub.docker.com/repository/docker/tushar2k5/my_image/general"
     elif task_id in ["GA2.9"]:
@@ -269,7 +269,7 @@ async def receive_question(question: str = Form(...), file: UploadFile = File(No
         answer = "https://tds-ga4-3.vercel.app/api/outline"
     elif task_id in ["GA4.8"]:
         answer = GA4_8(question)
-        # answer = "https://github.com/Telvinvarghese/Test"
+        # answer = "https://github.com/Rajesh4Github/Test"
     elif task_id in ["GA4.9"]:
         if file:
             print(file)
